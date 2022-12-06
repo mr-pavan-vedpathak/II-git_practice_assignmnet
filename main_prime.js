899af816ba6548d6566a50d46f8162d639347bad
pavan Vedpathak <119420282+mr-pavan-vedpathak@users.noreply.github.com>
2022-12-06 23:49:43 +0530

    Update main_prime.js
    changes made by member2

--- main_prime.js
+++ main_prime.js
@@ -1,1 +1,15 @@
 //main code merge here
+function prime_num(num){
+    let count=0;
+    for(let i=0;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+        if(count==2){
+            console.log("prime number");
+        }
+        else{
+            console.log("not prime")
+        }
+    }
+}
