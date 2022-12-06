39a784ee9a46af755d9049aaab401e63f4257ca3
unknown <0723shreya@gmail.com>
2022-12-06 23:24:26 +0530

    code fixed by member 2 again

--- main_prime.js
+++ main_prime.js
@@ -1,1 +1,16 @@
 //main code merge here
+function prime_num(num){
+    let count=0;
+    for(let i=1;i<=num;i++){
+        if(num%i==0){
+            count++;
+        }
+    }
+    if(count==2){
+        console.log("prime number");
+    }
+    else{
+        console.log("not prime");
+    }
+}
+prime_num(7);
